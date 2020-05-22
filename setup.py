92aea34d337c2e6efe58eab2aeb940d76dcf5111
kistee <christixd@safe-mail.net>
2020-05-22 23:17:31 +0200

    New upload to PyPi release 0.3

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name='easiercfg',
     packages=['easiercfg'],
-    version='0.2',
+    version='0.3',
     license='MIT',
     description='Python Module for Config-Files focused on easy usage.',
     author='kistee',
     url='https://github.com/kistee/easiercfg',
-    download_url='https://github.com/kistee/easiercfg/archive/v_02.tar.gz',
+    download_url='https://github.com/kistee/easiercfg/archive/v_03.tar.gz',
     keywords=['config', 'cfg', 'settings'],
     install_requires=[],
     classifiers=[
